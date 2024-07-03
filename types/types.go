316d0012bc80be53ef1651a5233c645952c0d748
Aidan Timson <contact@timmo.xyz>
2024-07-03 15:48:29 +0100

    Setup message service

--- types/types.go
+++ types/types.go
@@ -8,3 +8,8 @@ type Error struct {
 type Message struct {
 	Message string `json:"message"`
 }
+
+type Success struct {
+	Succeeded bool   `json:"succeeded"`
+	Message   string `json:"message"`
+}
--- websocket/websocket.go
+++ websocket/websocket.go
@@ -14,6 +14,8 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
+var connectedClients []*websocket.Conn
+
 func sendMessage(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -21,17 +23,21 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
+
+	// Add client to connectedClients
+	connectedClients = append(connectedClients, c)
+
 	for {
-		mt, message, err := c.ReadMessage()
+		mt, messageIn, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
-		log.Printf("recv: %s", message)
+		log.Printf("recv: %s", messageIn)
 
 		// Parse JSON
 		var data map[string]interface{}
-		err = json.Unmarshal(message, &data)
+		err = json.Unmarshal(messageIn, &data)
 		if err != nil {
 			log.Println("Error parsing JSON:", err)
 			// Send error message
@@ -48,9 +54,63 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 		// Print JSON
 		log.Printf("data: %v", data)
 
-		err = c.WriteMessage(mt, message)
+		// Validate JSON is of type Message
+		if _, ok := data["message"]; !ok {
+			log.Println("Error: JSON is not of type Message")
+			// Send error message
+			resp := types.Error{Message: "Error: JSON is not of type Message", Error: ""}
+			message, err := json.Marshal(resp)
+			if err != nil {
+				log.Println("Error marshalling JSON:", err)
+				break
+			}
+			c.WriteMessage(mt, message)
+			break
+		}
+
+		// TODO: Do something with the Message data
+
+		// Convert JSON to bytes
+		messageIn, err = json.Marshal(data)
+		if err != nil {
+			log.Println("Error marshalling JSON:", err)
+			break
+		}
+
+		// Send message to all clients
+		for _, client := range connectedClients {
+			// Don't send message to the client that sent it
+			if client == c {
+				continue
+			}
+
+			err = client.WriteMessage(mt, messageIn)
+			if err != nil {
+				log.Println("Error writing message:", err)
+				break
+			}
+		}
+
+		// Send success message
+		resp := types.Success{Succeeded: true, Message: "Message sent"}
+		messageOut, err := json.Marshal(resp)
+		if err != nil {
+			log.Println("Error marshalling JSON:", err)
+			break
+		}
+
+		// Send success message to client
+		err = c.WriteMessage(mt, messageOut)
 		if err != nil {
 			log.Println("Error writing message:", err)
+			break
+		}
+	}
+
+	// Remove client from connectedClients
+	for i, client := range connectedClients {
+		if client == c {
+			connectedClients = append(connectedClients[:i], connectedClients[i+1:]...)
 			break
 		}
 	}
