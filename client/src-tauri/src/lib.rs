51f6b912060e52a537c5b6a319c89c5450f87a87
Aidan Timson <contact@timmo.xyz>
2024-07-06 16:33:29 +0100

    Cleanup

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,10 +1,8 @@
-use tauri::Manager;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)] // Only include this code on debug builds
             {
                 let window = app.get_webview_window("main").unwrap();
