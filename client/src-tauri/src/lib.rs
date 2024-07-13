e77d5f1242f1fa0830e684773cf67de869d2e004
Aidan Timson <contact@timmo.xyz>
2024-07-13 11:48:20 +0100

    Setup tray icon

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
-use tauri::Manager;
+use tauri::{
+    menu::{MenuBuilder, MenuItemBuilder, PredefinedMenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconEvent},
+    Manager,
+};
 
 pub fn run() {
     tauri::Builder::default()
@@ -6,9 +10,36 @@ pub fn run() {
         .setup(|app| {
             #[cfg(debug_assertions)] // Only include this code on debug builds
             {
+                // Open devtools on start
                 let window = app.get_webview_window("main").unwrap();
                 window.open_devtools();
                 window.close_devtools();
+
+                // TODO: Connect to websocket as a headless client
+
+                // Setup tray menu
+                let separator = PredefinedMenuItem::separator(app)?;
+                let show_settings =
+                    MenuItemBuilder::with_id("show_settings", "Open settings").build(app)?;
+                let exit = MenuItemBuilder::with_id("exit", "Exit").build(app)?;
+
+                let menu = MenuBuilder::new(app)
+                    .items(&[&show_settings, &separator, &exit])
+                    .build()?;
+
+                // Setup tray icon
+                let tray = app.tray_by_id("main").unwrap();
+                tray.set_menu(Some(menu))?;
+                tray.on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        // TODO: Send a message to the webview
+                    }
+                });
             }
             Ok(())
         })
